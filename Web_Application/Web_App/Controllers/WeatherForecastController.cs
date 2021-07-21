3f479a644a8e2f4230fe26440a534332916be7fe
Павел Ремизов <remizov@rightway.games>
2021-07-22 00:51:08 +0300

    Домашнее задание к уроку 2

---
+++ Web_Application/Web_App/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Web_App.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ServiceProperties _settings;
+
+        public WeatherForecastController(IOptions<ServiceProperties> options, ILogger<WeatherForecastController> logger)
+        {
+            _settings = options.Value;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = $"{Summaries[rng.Next(Summaries.Length)]} {_settings.Host}"
+            })
+            .ToArray();
+        }
+    }
+}
